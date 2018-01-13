93375573988578efb5f27633d1bdf721f0dd0468
Ivan Pandzic <ivan.pandzic@gmail.com>
2018-01-13 20:11:39 +0100

    Created Movie model

--- movies/core/models.py
+++ movies/core/models.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    """
+    Describes a movie with properties such as title, year, etc.
+    """
+    title = models.CharField(max_length=1024)
+    content_rating = models.CharField(max_length=1024)
+    movie_url = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return self.title
