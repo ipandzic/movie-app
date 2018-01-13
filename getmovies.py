0c1b8e735784cc85adb179df4744a6d8b1e97df3
Ivan Pandzic <ivan.pandzic@gmail.com>
2018-01-13 10:33:43 +0100

    Renamed .py script

--- getmovies.py
+++ getmovies.py
@@ -1,13 +1,13 @@
 import urllib2
+from bs4 import BeautifulSoup
+
 
 my_url = "https://www.rottentomatoes.com/browse/opening/"
 
 page = urllib2.urlopen(my_url)
 
-from bs4 import BeautifulSoup
 soup = BeautifulSoup(page, 'html.parser')
 
-import re
 
 x = soup.findAll("script", {"id": "jsonLdSchema"})
 
