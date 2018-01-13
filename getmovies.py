250af1e12441a90a2a5d0403ce5e955db90a1c2a
Ivan Pandzic <ivan.pandzic@gmail.com>
2018-01-13 13:22:15 +0100

    Modified list

--- getmovies.py
+++ getmovies.py
@@ -11,7 +11,12 @@ soup = bs4.BeautifulSoup(request, 'html.parser')
 x = soup.findAll("script", {"id": "jsonLdSchema"})
 
 text = x[0].get_text()
-match = re.findall(r'\[.+\]', text)[0]
+
+match = re.findall(r'\[.+\]', text)[0][1:-1]
+match = match.replace("},{", "};{")
 
 
-print(match)
+match_list = match.split(';')
+
+for item in match_list:
+    print(item)
