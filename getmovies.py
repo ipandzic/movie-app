60cbe90375cf9864acb3df151d5aacede055c138
Ivan Pandzic <ivan.pandzic@gmail.com>
2018-01-13 12:03:42 +0100

    Found list

--- getmovies.py
+++ getmovies.py
@@ -1,6 +1,6 @@
 import urllib.request
 import bs4
-
+import re
 
 url = "https://www.rottentomatoes.com/browse/opening/"
 
@@ -10,4 +10,8 @@ soup = bs4.BeautifulSoup(request, 'html.parser')
 
 x = soup.findAll("script", {"id": "jsonLdSchema"})
 
-print(x)
+text = x[0].get_text()
+match = re.findall(r'\[.+\]', text)[0]
+
+
+print(match)
