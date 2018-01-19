1ff3b09df93eee0654456239ba9befe4ca775435
Ivan Pandzic <ivan.pandzic@gmail.com>
2018-01-19 21:14:02 +0100

    Modified admin

--- movies/core/admin.py
+++ movies/core/admin.py
@@ -5,5 +5,14 @@ from django.contrib import admin
 
 from .models import Movie
 
-# Register your models here.
-admin.site.register(Movie)
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ["title", "production_company", "rating"]
+    list_filter = ["production_company"]
+    search_fields = ["title", "production_company"]
+
+    class Meta:
+        model = Movie
+
+
+admin.site.register(Movie, MovieAdmin)
