685523db57219d7527044b3fbfa988305641f8f7
Ivan Pandzic <ivan.pandzic@gmail.com>
2018-01-20 09:57:28 +0100

    Added urls and views

--- movies/core/views.py
+++ movies/core/views.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Movie
+
+
+def index(request):
+    all_movies = Movie.objects.all()
+    html = ''
+    for movie in all_movies:
+        url = '/core/' + str(movie.id) + '/'
+        html += '<a href="' + url + '">' + movie.title + '</a><br>'
+    return HttpResponse(html)
+
+
+def detail(request, movie_id):
+    return HttpResponse("<h2>Details for Movie id: " + str(movie_id) + "</h2>")
