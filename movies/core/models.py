6003b7c60c7b03faa8d972c91f7ca7a83c1c826c
Ivan Pandzic <ivan.pandzic@gmail.com>
2018-01-21 21:27:29 +0100

    Modified models in models.py

--- movies/core/models.py
+++ movies/core/models.py
@@ -10,11 +10,11 @@ class Movie(models.Model):
     Describes a movie with properties such as title, production_company, etc.
     """
     position = models.IntegerField(blank=True, null=True)
-    title = models.CharField(max_length=1024, blank=True, null=True)
-    production_company = models.CharField(max_length=1024, blank=True, null=True)
+    title = models.CharField(max_length=1024)
+    production_company = models.CharField(max_length=1024, blank=True)
     rating = models.IntegerField(blank=True, null=True)
-    movie_url = models.CharField(max_length=1024, blank=True, null=True)
-    image_url = models.CharField(max_length=1024, blank=True, null=True)
+    movie_url = models.CharField(max_length=1024, blank=True)
+    image_url = models.CharField(max_length=1024, blank=True)
 
     def get_absolute_url(self):
         return reverse('core:detail', kwargs={'pk': self.pk})
