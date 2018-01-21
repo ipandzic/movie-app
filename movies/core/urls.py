de4dc2ae94ffe52323196015984f5405adb81c41
Ivan Pandzic <ivan.pandzic@gmail.com>
2018-01-21 09:45:41 +0100

    Updated design

--- movies/core/urls.py
+++ movies/core/urls.py
@@ -7,5 +7,7 @@ app_name = 'core'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^movie/add/$', views.MovieCreate.as_view(), name="movie-add")
+    url(r'^movie/add/$', views.MovieCreate.as_view(), name="movie-add"),
+    url(r'^movie/(?P<pk>[0-9]+)/$', views.MovieUpdate.as_view(), name="movie-update"),
+    url(r'^movie/(?P<pk>[0-9]+)/delete/$', views.MovieDelete.as_view(), name="movie-delete")
 ]
--- movies/core/views.py
+++ movies/core/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 from .models import Movie
 
 
@@ -22,3 +23,13 @@ class DetailView(generic.DetailView):
 class MovieCreate(CreateView):
     model = Movie
     fields = ['position', 'title', 'production_company', 'rating', 'movie_url', 'image_url']
+
+
+class MovieUpdate(UpdateView):
+    model = Movie
+    fields = ['position', 'title', 'production_company', 'rating', 'movie_url', 'image_url']
+
+
+class MovieDelete(DeleteView):
+    model = Movie
+    success_url = reverse_lazy('core:index')
