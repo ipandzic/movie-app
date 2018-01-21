27406e12c8c89284f7ea9f2f933641427b2626a7
Ivan Pandzic <ivan.pandzic@gmail.com>
2018-01-21 21:15:31 +0100

    Modified models.py

--- movies/core/models.py
+++ movies/core/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 class Movie(models.Model):
     """
-    Describes a movie with properties such as title, year, etc.
+    Describes a movie with properties such as title, production_company, etc.
     """
     position = models.IntegerField(blank=True, null=True)
     title = models.CharField(max_length=1024, blank=True, null=True)
@@ -19,5 +19,5 @@ class Movie(models.Model):
     def get_absolute_url(self):
         return reverse('core:detail', kwargs={'pk': self.pk})
 
-    def __str__(self):
+    def __unicode__(self):
         return self.title
